955f8e114bfb9e9fe9c5075d2f4806690332b9cd
Raphael Ferreira <raphana77@gmail.com>
2023-05-30 20:17:23 -0300

    Atualizando o estilo e posicionamentos

---
+++ questions.js
@@ -0,0 +1,101 @@
+let questions = [
+    {
+        question: 'Qual a seleção com mais títulos?',
+        options: [
+            'Alemanha',
+            'Brasil',
+            'Itália',
+            'Argentina'
+        ],
+        answer: 1
+    },
+    {
+        question: 'Quantos títulos o Brasil tem?',
+        options: [
+            '6',
+            '4',
+            '5',
+            '9'
+        ],
+        answer: 2
+    },
+    {
+        question: 'Quem é o maior artilheiro??',
+        options: [
+            'Pelé',
+            'Lionel Messi',
+            'Ronaldo Nazário',
+            'Miroslave Klose'
+        ],
+        answer: 3
+    },
+    {
+        question: 'Quem é o maior assistente?',
+        options: [
+            'Zinedine Zidane',
+            'Pelé',
+            'Maradona e Messi',
+            'Johan Cruiff'
+        ],
+        answer: 2
+    },
+    {
+        question: 'Qual jogador fez mais gols em uma edição?',
+        options: [
+            'Justi Fantino',
+            'Ronaldo Nazário',
+            'Pelé',
+            'Messi'
+        ],
+        answer: 0
+    },
+    {
+        question: 'Quando a copa do Mundo foi criada?',
+        options: [
+            '1950',
+            '1930',
+            '1925',
+            '1958'
+        ],
+        answer: 1
+    },
+    {
+        question: 'Qual jogador disputou mais edições?',
+        options: [
+            'Cafú',
+            'Antônio Carbajal',
+            'Diego Maradona',
+            'Pelé'
+        ],
+        answer: 1
+    },
+    {
+        question: 'Qual jogador tem mais títulos?',
+        options: [
+            'Pelé',
+            'Garrincha',
+            'Ronaldo Nazário',
+            'Diego Maradona'
+        ],
+        answer: 0
+    },
+    {
+        question: 'Qual a maior goleada?',
+        options: [
+            'Hungria x El Salvador (1982)',
+            'Hungria x Coreia do Sul (1954)',
+            'Iugoslávia x Zaire (1974)',
+            'Suécia x Cuba (1938)'
+        ],
+        answer: 0
+    },
+    {
+        question: 'Quais os artilheiros das últimas 3 edições?',
+        options: [
+            'Miroslav Klose - Gerd Müller - Just Fontaine ',
+            'Lionel Messi - Kylian Mbappé - Jürgen Klinsmann',
+            'Miroslav Klose - Lionel Messi - Kylian Mbappé'
+        ],
+        answer: 2
+    },
+];
